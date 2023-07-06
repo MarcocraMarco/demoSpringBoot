3dd18c314236f9ccbafb8918005367434e39572b
Marco Canu <nome.marco@icloud.com>
2023-07-06 09:58:00 +0200

    jpa tags to student

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,14 +2,21 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
     @NonNull
-    private Long  id;
+    private Long id;
     private String name;
     private String email;
     private LocalDate dateOfBirth;
